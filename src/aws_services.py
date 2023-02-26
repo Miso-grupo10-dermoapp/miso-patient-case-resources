64dd88e8f6c2d6d5994cddf6113a59d3a78f090e
fepego-penag <cf.pena1138@uniandes.edu.co>
2023-02-26 08:17:33 -0500

    adding test path

--- src/aws_services.py
+++ src/aws_services.py
@@ -9,7 +9,6 @@ bucket_name = "cases-resources"
 def upload_file(case_id, file_name, file):
     try:
         client = boto3.client('s3')
-        client.put_object(Body=file, Bucket=bucket_name, Key= case_id + '/' + file_name,
-                          ContentType='image/jpeg')
+        client.put_object(Body=file, Bucket=bucket_name, Path= case_id + '/' + file_name, ContentType='image/jpeg')
     except Exception as e:
         raise Exception('cannot upload file on s3: {0}'.format(e))
