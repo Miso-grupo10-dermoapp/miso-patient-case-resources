d7fa6cfa56b59058d882ce13d4fc66b204571cd4
fepego-penag <cf.pena1138@uniandes.edu.co>
2023-02-26 08:35:42 -0500

    testing image upload

--- src/aws_services.py
+++ src/aws_services.py
@@ -1,5 +1,7 @@
 import boto3
-from boto3.dynamodb.conditions import Key
+import io
+
+from PIL.Image import Image
 
 import app
 
@@ -9,6 +11,7 @@ bucket_name = "cases-resources"
 def upload_file(case_id, file_name, file):
     try:
         client = boto3.client('s3')
-        client.put_object(Body=file, Bucket=bucket_name, Key= case_id + '/' + file_name, ContentType='image/jpeg')
+        image = Image.frombytes('RGBA', (128,128), file, 'raw')
+        client.put_object(Body=image, Bucket=bucket_name, Key= case_id + '/' + file_name, ContentType='image/jpeg')
     except Exception as e:
         raise Exception('cannot upload file on s3: {0}'.format(e))
