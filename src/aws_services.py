79168c95b3547f8652ac2cbc8b3b76eeb53ff792
fepego-penag <cf.pena1138@uniandes.edu.co>
2023-02-26 08:41:11 -0500

    testing upload array

--- src/aws_services.py
+++ src/aws_services.py
@@ -1,9 +1,5 @@
 import boto3
-import io
 
-from PIL.Image import Image
-
-import app
 
 bucket_name = "cases-resources"
 
@@ -11,7 +7,6 @@ bucket_name = "cases-resources"
 def upload_file(case_id, file_name, file):
     try:
         client = boto3.client('s3')
-        image = Image.frombytes('RGBA', (128,128), file, 'raw')
-        client.put_object(Body=image, Bucket=bucket_name, Key= case_id + '/' + file_name, ContentType='image/jpeg')
+        client.put_object(Body=file, Bucket=bucket_name, Key= case_id + '/' + file_name, ContentType='image/jpeg')
     except Exception as e:
         raise Exception('cannot upload file on s3: {0}'.format(e))
